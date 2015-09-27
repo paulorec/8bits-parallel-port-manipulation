622d55d9cd477b2d3ff891256194f3de7b03727d
paulo <paulorec@gmail.com>
2015-09-26 21:06:06 -0300

    First files

---
+++ 8bits/src/oitobits/controlador/ControleOpEmLoop.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oitobits.controlador;
+
+import oitobits.modelo.Paralela;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import oitobits.modelo.ConexaoParalela;
+import oitobits.visao.JanelaOpEmLoop;
+
+/**
+ *
+ * @author paulo
+ */
+
+@Deprecated
+public class ControleOpEmLoop implements ActionListener {
+ JanelaOpEmLoop jol;
+ Loop thread;
+ 
+  public void startJanela()
+  {
+   jol = new JanelaOpEmLoop(this);
+  }
+
+  public void actionPerformed(ActionEvent e) {
+   if(e.getSource() == this.jol.iniciar)
+   {
+      int valores[] = this.jol.getInteiros();
+     //Paralela paralela = new Paralela();
+      int porta = jol.getPorta();
+      int intervalo = jol.getIntervalo();
+
+      //Status status = new Status(porta, jol.status);
+      //status.start();
+      if(porta != 0 && intervalo  != 0)
+      {
+       this.thread = new Loop(valores, porta, intervalo);
+       this.thread.iniciado = true;
+       this.thread.start();
+       this.jol.iniciar.setEnabled(false);
+       this.jol.parar.setEnabled(true);
+      }
+   }
+   if(e.getSource() == this.jol.parar)
+   {
+     if(this.thread != null) this.thread.iniciado = false;
+     this.jol.iniciar.setEnabled(true);
+     this.jol.parar.setEnabled(false);
+   }
+  }
+    public static void main(String[] args) {
+        ControleOpEmLoop copLoop = new ControleOpEmLoop();
+        copLoop.startJanela();
+    }
+}
+class Loop extends Thread
+{
+ private int tempoEspera;
+ private int[] sequenciaValores;
+ private Paralela paralela;
+ private int porta;
+ public boolean iniciado = false;
+ 
+ public Loop(int[] valores, int porta, int tempo)
+ {
+  this.sequenciaValores = valores;
+  this.tempoEspera = tempo;
+  //this.paralela = new Paralela();
+  this.porta = porta;
+ }
+
+ @Override
+ public void run()
+ {
+  try
+  {
+   while(this.iniciado)
+   {
+    for (int i : sequenciaValores)
+    {
+     this.paralela.setPorta(porta);
+     this.paralela.escrever(i);
+     Thread.sleep(tempoEspera);
+    }
+   }
+  }
+  catch(Exception e)
+  {
+   System.out.println("Excecao disparada por thread ");
+   e.printStackTrace();   
+  }
+ }
+}
---
+++ 8bits/src/oitobits/controlador/Fachada.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oitobits.controlador;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import oitobits.modelo.Dispositivo;
+import oitobits.modelo.Paralela;
+import oitobits.modelo.SMS;
+import oitobits.visao.JanelaDesktop;
+
+/**
+ *
+ * @author paulo
+ */
+public class Fachada {
+
+ /**
+  * Retorna um array de dispositivos associados a porta paralela
+  * @return Dispostivo[]
+  */
+ public static Dispositivo[] getDispositivos()
+ {
+   Dispositivo[] dispositivos = new Dispositivo[8] ;
+   for(int x=0;x<dispositivos.length;x++)
+   {
+       dispositivos[x] = new Dispositivo();
+   }
+    dispositivos[0].setNome("Dispositivo 1");
+    dispositivos[1].setNome("Dispositivo 2");
+    dispositivos[2].setNome("Dispositivo 3");
+    dispositivos[3].setNome("Dispositivo 4");
+    dispositivos[4].setNome("Dispositivo 5");
+    dispositivos[5].setNome("Dispositivo 6");
+    dispositivos[6].setNome("Dispositivo 7");
+    dispositivos[7].setNome("Dispositivo 8");
+
+
+    dispositivos[0].setPino(1);
+    dispositivos[1].setPino(2);
+    dispositivos[2].setPino(3);
+    dispositivos[3].setPino(4);
+    dispositivos[4].setPino(5);
+    dispositivos[5].setPino(6);
+    dispositivos[6].setPino(7);
+    dispositivos[7].setPino(8);
+
+    return dispositivos;
+ }
+
+ /**
+  * Inicia a interface gráfica da aplicação.
+  */
+ public static void iniciarInterface()
+ {
+     JanelaDesktop jd = new JanelaDesktop();
+ }
+
+ /**
+  * Metodo principal da aplicação.
+  * @param argumentos
+  */
+ public static void main(String[] argumentos)
+ {
+  try
+  {
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    Fachada.resetParalela();
+    Paralela.getInstancia().setDispositivos(Fachada.getDispositivos());
+    Fachada.iniciarInterface();
+  }
+  catch(Exception e) { e.printStackTrace();}
+     
+ }
+
+ public static void showErro(String erro)
+ {
+  JOptionPane.showMessageDialog(null, erro);
+ }
+
+ /**
+  * Envia uma mensagem via celular para o número especificado.
+  * @param numero
+  * @param texto
+  */
+ public static void enviarSms(String numero, String texto)
+ {
+  try
+  {
+   SMS.enviar(numero, texto);
+  }
+  catch(Exception e)
+  {
+   Fachada.showErro("Não foi possivel enviar SMS: " + e.getMessage());
+   e.printStackTrace();
+  }
+ }
+
+ /**
+  * Desliga todos os dispositivos associados a paralela.
+  */
+ public static void resetParalela()
+ {
+   Paralela.getInstancia().setPorta(888);
+   Paralela.getInstancia().escrever(0);
+ }
+}
